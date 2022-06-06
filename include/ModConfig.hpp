0dab12f5d6079307b7a66135afc0da4bc1d97f7b
JD Williams <jdwill239@gmail.com>
2022-06-06 11:10:01 -0400

    Add OAuth functionality for message creation.

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -4,8 +4,10 @@
 DECLARE_CONFIG(ModConfig,
 
     CONFIG_VALUE(Channel, std::string, "Channel Name", "darknight1050");
+    CONFIG_VALUE(OAuth, std::string, "OAuth Token", "ENTER_TOKEN_HERE");
 
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(Channel);
+        CONFIG_INIT_VALUE(OAuth);
     )
 )
